674ae1bcb4a203b75a7ab7a6e793226999c935f7
Bryan Turns <turnsbryan@gmail.com>
2025-05-14 19:16:43 -0600

    generalized cleaning function + better boulder grading

--- cleaning_utils.py
+++ cleaning_utils.py
@@ -1,7 +1,22 @@
 import pandas as pd
 import re
 
+
+def general_clean(routes_df):
+    routes_df["avg_rating"] = pd.to_numeric(routes_df["avg_rating"])
+    routes_df["rating_count"] = pd.to_numeric(routes_df["rating_count"])
+
+    routes_df = routes_df[routes_df["rating_count"] > 5]
+
+    return routes_df
+
+def ropes_only(routes_df):
+    routes_df = routes_df.dropna(subset=["grade"])
+    routes_df = routes_df[routes_df["grade"].str.contains(r"^[^V].*", regex=True)]
+    return routes_df
+
 def bouldering_only(routes_df):
+    routes_df = routes_df.dropna(subset=["grade"])
     routes_df = routes_df[routes_df["grade"].str.startswith("V")]
     return routes_df
 
@@ -11,7 +26,11 @@ def translate_boulder_grade(routes_df):
     return routes_df
 
 def translate_boulder_sign(grade_string):
-    grade = re.search(r"^\d{1,2}|[eE]|[bB]", grade_string).group()
+    grade = re.search(r"^\d{1,2}|[eE]|[bB]", grade_string)
+    if grade == None:
+        return "-1"
+    else: 
+        grade = grade.group()
     sign = re.search(r"\+|\-$", grade_string)
     if sign != None:
         sign = sign.group()
@@ -21,9 +40,9 @@ def translate_boulder_sign(grade_string):
     grade = int(grade)
 
     if sign == "+":
-        grade += 0.5
+        grade += 0.25
     elif sign == "-":
-        grade -= 0.5
+        grade -= 0.25
 
     return grade
     
